aa692237b0a6b25eb00c8c59ba81d56cf946bf92
aleksanderchugui <aleksandr.chugui@gmail.com>
2024-02-01 17:54:40 +0200

    some reels animation changes

--- assets/Script/reels/Reel.ts
+++ assets/Script/reels/Reel.ts
@@ -16,8 +16,15 @@ export default class Reel extends cc.Component {
     id = 0;
     @property({ type: [ReelSymbol] })
     items = [];
+    initialItemsY = [];
+    currentNodeY: number = null;
+    iteration: number = 0;
 
-    // onLoad () {}
+    onLoad () {
+        this.items.forEach(item => {
+            this.initialItemsY.push(item.node.y);
+        });
+    }
 
     start () {
 
@@ -36,9 +43,22 @@ export default class Reel extends cc.Component {
     startAnimation(stopSymbols: number[]): void {
         const onComplete = cc.tween().call(() => this.onAnimationComplete(stopSymbols));
         cc.tween(this.node)
-            .to(1, {y: -500})
+            .to(1, {y: -500}, {onUpdate: () => {
+                    this.onAnimationUpdate();
+                }
+            })
             .then(onComplete)
             .start();
+    }
+
+    onAnimationUpdate(): void {
+        if ((!this.currentNodeY && this.node.y < -125) ||
+            (this.currentNodeY < -125 && this.currentNodeY > -250 && this.node.y < -250) ||
+            (this.currentNodeY < -250 && this.currentNodeY > -375 && this.node.y < -375)) {
+            this.currentNodeY = this.node.y;
+            this.items[this.items.length - this.iteration - 1].node.y = this.initialItemsY[0] + Math.abs(this.currentNodeY);
+            this.iteration++;
+        }
     }
 
     onAnimationComplete(stopSymbols: number[]): void {
@@ -49,6 +69,11 @@ export default class Reel extends cc.Component {
             this.items[i].getComponent(cc.Sprite).spriteFrame = cc.assetManager.assets.find(a => a.name === imageName);
         }
         this.node.y = 0;
+        this.iteration = 0;
+        this.currentNodeY = null;
+        for (let i = 0; i < this.items.length - 1; i++) {
+            this.items[i].node.y = this.initialItemsY[i];
+        }
         if (this.id === 4) {
             this.node.dispatchEvent(new cc.Event.EventCustom('reelsStopped', true));
         }
